b1c901b5cb800072bf777e64ce8ee9c77813f0a3
Matt Bierbaum <matt.bierbaum@gmail.com>
2013-12-14 15:36:54 -0500

    stripped out entbody

--- entbody.js
+++ entbody.js
@@ -7,8 +7,6 @@ var fx=[];
 var fy=[];
 var r=[];
 var type=[];
-var col=[];
-var colavg;
 var ivor=[];
 var ivoravg;
 
@@ -18,8 +16,7 @@ var flick_noise = 6e3;
 var flick = flick_goal;
 
 // things we can change
-var n = 500;
-var pbc = [1,1];
+var n = 5;
 
 // neighborlist stuff
 var lx, ly;
@@ -48,7 +45,6 @@ var empty;
 var frame = 0;
 var keys = [0,0,0,0];
 var frameskip = 2;
-var colscale=25;
 var dodraw = true;
 var docircle = true;
 var showforce = true;
@@ -57,89 +53,29 @@ function rgb(r,g,b) {
     return 'rgb('+r+','+g+','+b+')';
 }
 
-function mymod(a, b){
-  return a - b*Math.floor(a/b) + b*(a<0);
-}
-
-function mod_rvec(a, b, p, image, iind){
-    image[iind] = 1;
-    if (b==0) {if (a==0) {image[iind]=0;} return 0;}
-    if (p != 0){
-        if (a>b)  {return a-b-1;}
-        if (a<0)  {return a+b+1;}
-    } else {
-        if (a>b)  {return b;}
-        if (a<0)  {return 0;}
-    }
-    image[iind] = 0;
-    return a;
-}
-
-function nbl_bin(){
-    for (var i=0; i<size[0]*size[1]; i++){
-        count[i] = 0;
-    }
-    for (var i=0; i<n; i++){
-        var indx = Math.floor(x[i]/lx * size[0]);
-        var indy = Math.floor(y[i]/ly * size[1]);
-        var tt = indx + indy*size[0];
-        cells[NMAX*tt + count[tt]] = i;
-        count[tt]++;
-    }
-}
-
 function update(){
-    colavg = 0.0;
-    var image = [0,0];
     for (var i=0; i<n; i++) {
-        col[i] = 0.0;
         fx[i] = 0.0; 
         fy[i] = 0.0;
-        var wx = 0.0; 
-        var wy = 0.0;
-        var neigh = 0;
 
-        var indx = Math.floor(x[i]/lx * size[0]);
-        var indy = Math.floor(y[i]/ly * size[1]);
-        for (var ttx=-1; ttx<=1; ttx++){
-        for (var tty=-1; tty<=1; tty++){ 
-            var goodcell = 1;
-            var tixx = mod_rvec(indx+ttx, size[0]-1, pbc[0], image, 0);
-            var tixy = mod_rvec(indy+tty, size[1]-1, pbc[1], image, 1);
-            if (pbc[0] < image[0]){ goodcell = 0; }
-            if (pbc[1] < image[1]){ goodcell = 0; }
+        for (var j=0; j<n; j++){
+            if (i==j) continue;
             
-            if (goodcell){
-                var cell = tixx + tixy*size[0];
-                for (var cc=0; cc<count[cell]; cc++){
-                    var j = cells[NMAX*cell + cc]; 
-                    var dx = x[j] - x[i]; if (image[0]) {dx += lx*ttx;}
-                    var dy = y[j] - y[i]; if (image[1]) {dy += ly*tty;}
-                    var l = Math.sqrt(dx*dx + dy*dy);
-                    if (l > 1e-6 && l < R){
-                        var r0 = (r[i]+r[j]);
-                        var f = (1-l/r0);
-                        var c0 = -epsilon * f*f * (l<r0);
-                        fx[i] += c0*dx;
-                        fy[i] += c0*dy;
-
-                        var tcol = c0*c0*dx*dx + c0*c0*dy*dy;//fx[i]*fx[i] + fy[i]*fy[i];
-                        col[i] += tcol;
-                    }
-                }
-            }   
-        } }
-        var vlen = (vx[i]*vx[i] + vy[i]*vy[i]);
-        var vhap = 0.0;
-        if (type[i]==1) { vhap = vhappy; } else { vhap = 0.0; }
-        if (vlen > 1e-6){
-            fx[i] += damp*(vhap - vlen)*vx[i]/vlen;
-            fy[i] += damp*(vhap - vlen)*vy[i]/vlen;
+            var dx = x[j] - x[i];
+            var dy = y[j] - y[i];
+            var l = Math.sqrt(dx*dx + dy*dy);
+            if (l > 1e-6 && l < R){
+                var r0 = (r[i]+r[j]);
+                var f = (1-l/r0);
+                var c0 = -epsilon * f*f * (l<r0);
+                fx[i] += c0*dx;
+                fy[i] += c0*dy;
+            }
         }
-
-        if (type[i] == 1){
-            fx[i] += noise * (Math.random()-0.5);
-            fy[i] += noise * (Math.random()-0.5);
+        var vlen = (vx[i]*vx[i] + vy[i]*vy[i]);
+        if (vlen > 1e-6){
+            fx[i] += -damp*vlen*vx[i]/vlen;
+            fy[i] += -damp*vlen*vy[i]/vlen;
         }
 
         if (type[i] == 1){
@@ -148,34 +84,26 @@ function update(){
             if (keys[2] == 1) {fx[i] -= 5.0;}
             if (keys[3] == 1) {fx[i] += 5.0;}
         }
-
-        //var tcol = fx[i]*fx[i] + fy[i]*fy[i];
-        //col[i] += tcol;
-        colavg += col[i];
     }
 
     for (var i=0; i<n; i++){
         vx[i] += fx[i] * gdt;
         vy[i] += fy[i] * gdt;
 
+        var vmax = 1.0;
+        if (vx[i] > vmax)  vx[i] = vmax;
+        if (vx[i] < -vmax) vx[i] = -vmax;
+        if (vy[i] > vmax)  vy[i] = vmax;
+        if (vy[i] < -vmax) vy[i] = -vmax;
+
         x[i] += vx[i] * gdt;
         y[i] += vy[i] * gdt;
     
-        if (pbc[0] == 0){
-            if (x[i] >= lx){x[i] = 2*lx-x[i]; vx[i] *= -1;}
-            if (x[i] < 0)  {x[i] = -x[i];     vx[i] *= -1;}
-        } else {
-            if (x[i] >= lx || x[i] < 0) {x[i] = mymod(x[i], lx);}
-        }
-        if (pbc[1] == 0){
-            if (y[i] >= ly){y[i] = 2*ly-y[i]; vy[i] *= -1;}
-            if (y[i] < 0)  {y[i] = -y[i];     vy[i] *= -1;}
-        } else {
-            if (y[i] >= ly || y[i] < 0) {y[i] = mymod(y[i], ly);}
-        } 
-
+        if (x[i] >= lx){x[i] = 2*lx-x[i]; vx[i] *= -1;}
+        if (x[i] < 0)  {x[i] = -x[i];     vx[i] *= -1;}
+        if (y[i] >= ly){y[i] = 2*ly-y[i]; vy[i] *= -1;}
+        if (y[i] < 0)  {y[i] = -y[i];     vy[i] *= -1;}
     }
-    colavg /= n;
 }
 
 function draw_all(x, y, r, lx, ly, cw, ch, ctx, ctx2) {
@@ -194,12 +122,9 @@ function draw_all(x, y, r, lx, ly, cw, ch, ctx, ctx2) {
         var cr,cg,cb;
         if (type[i] == 0){
             if (showforce == true){
-                cr = Math.abs(col[i]/colscale);
-                if (cr < 0) {cr = 0.0;}
-                if (cr > 1) {cr = 1.0;}
-                cr = 180;//Math.floor(255*cr);
-                cg = 180;//cr;
-                cb = 180;//cr;
+                cr = 180;
+                cg = 180;
+                cb = 180;
             } else {
                 cr = 50;
                 cg = 50;
@@ -220,15 +145,11 @@ function draw_all(x, y, r, lx, ly, cw, ch, ctx, ctx2) {
     flick = Math.max(flick, 30);
 
     var radgrad = ctx.createRadialGradient(sx*x[0], sy*y[0], 0, sx*x[0], sy*y[0], flick);
-    radgrad.addColorStop(0, 'rgba(0,0,0,0.0)');
-    radgrad.addColorStop(0.5, 'rgba(0,0,0,0.5)');
-    radgrad.addColorStop(1, 'rgba(0,0,0,1)');
+    radgrad.addColorStop(0, 'rgba(150,0,0,0.0)');
+    radgrad.addColorStop(0.5, 'rgba(0,0,0,150)');
+    radgrad.addColorStop(1, 'rgba(0,0,0,255)');
     ctx2.fillStyle = radgrad;
     ctx2.fillRect(0,0,c.height,c.width);
-}
-
-function calc_sidelength(){
-    return Math.floor(0.99*Math.sqrt(Math.PI*radius*radius*n));
 }
 
 function init_sidelength(L){
@@ -264,7 +185,6 @@ function init_empty(){
         vy.push(0.0);
         fx.push(0.0);
         fy.push(0.0);
-        col.push(0.0);
     }
 }
 
@@ -316,7 +236,6 @@ var tick = function(T) {
         ctx2.fillRect(0,0,c.width,c.height);
         for (var i=0; i<frameskip; i++){
             frame++;
-            nbl_bin();
             update();
         }
  
@@ -338,7 +257,7 @@ var init = function() {
     ctx2 = c2.getContext('2d');
 
     init_empty();
-    init_sidelength(calc_sidelength());
+    init_sidelength(50);
     init_circle(frac);
 
     document.body.addEventListener('keyup', function(ev) {
